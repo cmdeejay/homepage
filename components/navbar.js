aa3eac8826edd0029ee5f6770924c9b7dd0487c2
cmdeejay@charmyminchen@gmail.com <min@be-humble.local>
2022-12-12 23:31:38 +0400

    updated homepage

--- components/navbar.js
+++ components/navbar.js
@@ -7,11 +7,6 @@ import {
   Stack,
   Heading,
   Flex,
-  Menu,
-  MenuItem,
-  MenuList,
-  MenuButton,
-  IconButton,
   useColorModeValue
 } from '@chakra-ui/react'
 import { HamburgerIcon } from '@chakra-ui/icons'
